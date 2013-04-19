54b515051d9d682bad13e6f8b5ef121c43c2ccd7
Pedro Cambra <pedro.cambra@gmail.com>
2013-04-19 15:54:22 +0100

    Issue #1616054 by pcambra, amateescu: Properly implement the selection plugin for Entityreference.

--- plugins/selection/EntityReference_SelectionHandler_Generic_commerce_coupon.class.php
+++ plugins/selection/EntityReference_SelectionHandler_Generic_commerce_coupon.class.php
@@ -1,14 +1,4 @@
 <?php
-
-$plugin = array(
-  'title' => t('Coupon simple'),
-  'handler' => array(
-    'class' => 'EntityReference_SelectionHandler_Generic_commerce_coupon',
-  ),
-);
-
-include_once drupal_get_path('module', 'entityreference') . '/plugins/selection/EntityReference_SelectionHandler_Generic.class.php';
-
 /**
  * Extend the entity reference selection class to use the coupon code for
  * building the EFQ.
@@ -16,7 +6,7 @@ include_once drupal_get_path('module', 'entityreference') . '/plugins/selection/
 class EntityReference_SelectionHandler_Generic_commerce_coupon extends EntityReference_SelectionHandler_Generic {
   public function buildEntityFieldQuery($match = NULL, $match_operator = 'CONTAINS') {
     $query = parent::buildEntityFieldQuery($match, $match_operator);
-    
+
     // If there's a match, query the coupon code.
     if (isset($match)) {
       $query->fieldCondition('commerce_coupon_code', 'value', $match, $match_operator);
@@ -28,5 +18,5 @@ class EntityReference_SelectionHandler_Generic_commerce_coupon extends EntityRef
     $wrapper = entity_metadata_wrapper('commerce_coupon', $entity);
     return $wrapper->commerce_coupon_code->value();
   }
-  
+
 }
